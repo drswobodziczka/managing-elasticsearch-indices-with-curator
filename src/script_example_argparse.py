6a1eeeed8562210ca857ec5b1138225a5e4d6d2a
Wawrzkowicz, Rafal <ext-rafal.wawrzkowicz@f-secure.com>
2020-07-22 18:02:27 +0200

    feat: parsing cmd arguments -- argsparse example -- added main

--- src/script_example_argparse.py
+++ src/script_example_argparse.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
 
 import argparse
+import sys
 
-# Initialize parser
-parser = argparse.ArgumentParser(description="Hello world scripting with argsparse!")
+def main(argv):
+  # Initialize parser
+  parser = argparse.ArgumentParser(description="Hello world scripting with argsparse!")
 
-# Adding optional argument
-parser.add_argument("--host", type=str, required=True, help="AWS Elasticsearch cluster url")
-parser.add_argument("--aws-profile", type=str, required=True, choices=["default, saml"], help=".aws/credentials profile name to take AWS credentials from")
-parser.add_argument("--index-regex", type=str, required=True, help="reindex only indices matching INDEX_REGEX")
-parser.add_argument("--older-than", type=int, default=1, help="reindex only indices older than OLDER_THAN, in days")
-parser.add_argument("--dry-run", type=bool, default=True, help="reindex in DRY-RUN mode, not affecting elasticsearch cluster")
+  # Adding optional argument
+  parser.add_argument("--host", type=str, required=True, help="AWS Elasticsearch cluster url")
+  parser.add_argument("--aws-profile", type=str, required=True, choices=["default", "saml"], help=".aws/credentials profile name to take AWS credentials from")
+  parser.add_argument("--index-regex", type=str, required=True, help="reindex only indices matching INDEX_REGEX")
+  parser.add_argument("--older-than", type=int, default=1, help="reindex only indices older than OLDER_THAN, in days")
+  parser.add_argument("--dry-run", type=bool, default=True, help="reindex in DRY-RUN mode, not affecting elasticsearch cluster")
 
-# Read arguments from command line
-args = parser.parse_args()
+  # Read arguments from command line
+  args = parser.parse_args()
 
-print(args)
+  print(args)
+
+if __name__ == "__main__":
+  main(sys.argv[1:])
